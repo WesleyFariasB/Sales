efbb7437639c3ee92af18f5e00c801b6edc77f9c
Wesley Farias <wesley_Farias7@outlook.com>
2024-08-14 13:32:26 -0300

    att2

--- script.js
+++ script.js
@@ -89,3 +89,24 @@ function prevSlide() {
   changeSlide(slideIndex);
   changeAccordion(slideIndex);
 }
+
+
+let slideIndex = [1,1];
+let slideClassNames = ["slides"]
+showSlides(1, 0);
+showSlides(1, 1);
+
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
+}
+
+function showSlides(n, no) {
+  let i;
+  let x = document.getElementsByClassName(slideClassNames[no]);
+  if (n > x.length) {slideIndex[no] = 1}    
+  if (n < 1) {slideIndex[no] = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  x[slideIndex[no]-1].style.display = "block";  
+}
