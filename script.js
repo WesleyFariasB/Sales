7e0386f7f1785bac2e344f94bc68af25972cc5ae
Wesley Farias <wesley_Farias7@outlook.com>
2024-08-23 12:07:08 -0300

    v5

--- script.js
+++ script.js
@@ -157,3 +157,15 @@ for (i = 0; i < len; i++) {
     }
   });
 }
+
+function menuShow() {
+
+    let menuMobile = document.querySelector('.mobile-menu');
+    if (menuMobile.classList.contains('open')) {
+        menuMobile.classList.remove('open');
+        document.querySelector('.icon').scr = "img/menu_white_36dp.svg"
+    } else {
+      menuMobile.classList.add('open')
+      document.querySelector('.icon').scr = "img/close_white_36dp.svg"
+    }
+}
